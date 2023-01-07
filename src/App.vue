af79352c6ab274fedaba392f23cd30ce6f77fd0f
A-Apkhanov <apkhanov.a.s@gmail.com>
2023-01-07 17:25:28 +0600

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,118 @@
 <template>
-  <div id="app" class="flex flex-col min-h-screen">
-    <header class="bg-sky-700">
-      <div class="max-w-7xl mx-auto p-4">
+  <div
+    id="app"
+    class="flex flex-col min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500"
+  >
+    <header>
+      <div class="max-w-7xl mx-auto p-4 flex justify-center">
         <span class="text-sky-50 text-xl">MyPassword</span>
       </div>
     </header>
-    <main class="flex-auto flex justify-center items-center bg-sky-700">
+    <main class="flex-auto flex justify-center items-center">
       <form
-        class="flex flex-col gap-3 w-full max-w-xs m-2.5 p-2.5 rounded bg-blue-50"
+        class="flex flex-col gap-3 w-full max-w-xs m-2.5 p-5 rounded-2xl bg-slate-50"
         @submit.prevent="generatePassword"
       >
-        <label for="password">Password</label>
-        <input
-          type="text"
-          placeholder="Password"
-          id="password"
-          class="p-2"
-          v-model="password"
-        />
-        <label for="length">Length (4-64): {{ length }}</label>
-        <input
-          type="range"
-          id="length"
-          name="vol"
-          min="4"
-          max="64"
-          v-model="length"
-        />
+        <label
+          for="password"
+          class="absolute overflow-hidden w-px h-px p-0 border-0"
+        >
+          Password
+        </label>
+        <div class="flex relative">
+          <input
+            type="text"
+            placeholder="Password"
+            id="password"
+            class="flex-auto p-3 rounded-lg bg-slate-200 focus:bg-blue-600/10"
+            v-model="password"
+          />
+          <button
+            type="button"
+            class="group absolute inset-y-0 right-0 rounded-lg p-3 bg-transparent focus:outline-none focus:shadow-outline"
+            @click="copyPassword"
+            :disabled="isCopyDisabled"
+          >
+            <icon-done v-if="isCopyDone" />
+            <icon-copy v-else />
+          </button>
+        </div>
+        <div class="flex flex-col gap-2">
+          <label for="length">Length: {{ length }}</label>
+          <input
+            type="range"
+            id="length"
+            name="vol"
+            min="8"
+            max="64"
+            v-model="length"
+          />
+        </div>
         <div class="flex flex-col">
-          <div class="flex gap-2">
+          <div class="flex gap-2 items-center">
             <input
               type="checkbox"
               id="letters"
               name="letters"
               v-model="letters"
+              class="w-4 h-4"
             />
             <label for="letters">Letters</label>
           </div>
-          <div class="flex gap-2">
-            <input type="checkbox" id="case" name="case" v-model="difCase" />
+          <div class="flex gap-2 items-center">
+            <input
+              type="checkbox"
+              id="case"
+              name="case"
+              v-model="difCase"
+              class="w-4 h-4"
+            />
             <label for="case">Different case</label>
           </div>
-          <div class="flex gap-2">
-            <input type="checkbox" id="marks" name="marks" v-model="marks" />
+          <div class="flex gap-2 items-center">
+            <input
+              type="checkbox"
+              id="marks"
+              name="marks"
+              v-model="marks"
+              class="w-4 h-4"
+            />
             <label for="marks">Punctuation marks</label>
           </div>
-          <div class="flex gap-2">
+          <div class="flex gap-2 items-center">
             <input
               type="checkbox"
               id="numbers"
               name="numbers"
               v-model="numbers"
+              class="w-4 h-4"
             />
             <label for="numbers">Numbers</label>
           </div>
         </div>
-        <div class="flex gap-1">
+        <div class="flex flex-col gap-1">
           <button
             type="submit"
-            class="flex-auto p-1 bg-blue-500 rounded-sm text-sky-50 hover:bg-sky-600 active:bg-blue-400"
+            class="flex-auto p-2.5 bg-blue-600 rounded-full text-sky-50 hover:brightness-110 active:brightness-90"
           >
             Generate
-          </button>
-          <button
-            type="button"
-            class="flex-auto p-1 bg-blue-500 rounded-sm text-sky-50 hover:bg-sky-600 active:bg-blue-400"
-            @click="copyPassword"
-          >
-            Copy
           </button>
         </div>
       </form>
     </main>
-    <footer class="text-center bg-sky-700">
-      <span class="text-sky-50 text-xs">© 2023 Apkahnov Aleksand</span>
+    <footer class="text-center">
+      <span class="text-sky-50 text-xs">© 2023 Apkahnov Aleksandr</span>
     </footer>
   </div>
 </template>
 
 <script>
+import IconCopy from "./components/icons/IconCopy.vue";
+import IconDone from "./components/icons/IconDone.vue";
+
 export default {
   name: "App",
+  components: { IconDone, IconCopy },
   data() {
     return {
       password: "",
@@ -89,6 +121,8 @@ export default {
       difCase: true,
       marks: true,
       numbers: true,
+      isCopyDone: false,
+      isCopyDisabled: true,
     };
   },
   methods: {
@@ -109,7 +143,15 @@ export default {
       this.password = password;
     },
     copyPassword() {
-      navigator.clipboard.writeText(this.password);
+      navigator.clipboard.writeText(this.password).then(() => {
+        this.isCopyDone = true;
+        setTimeout(() => (this.isCopyDone = false), 500);
+      });
+    },
+  },
+  watch: {
+    password(newPassword) {
+      this.isCopyDisabled = newPassword === "";
     },
   },
 };
