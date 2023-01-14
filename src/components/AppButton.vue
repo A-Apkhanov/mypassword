e65fd30e3857ff3fe51fd6876a3a27d64fcea4c3
A-Apkhanov <apkhanov.a.s@gmail.com>
2023-01-14 14:34:24 +0600

    refactoring button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <button :type="type" :class="$style.button" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+button.button {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 10px;
+
+  border-radius: 9999px;
+
+  color: white;
+
+  background-color: #2a9d8f;
+
+  outline: none;
+
+  border: 2px solid transparent;
+
+  &:disabled {
+    filter: opacity(50%);
+  }
+
+  &:hover:not(:active):not(:disabled) {
+    filter: brightness(90%);
+  }
+
+  &:active:not(:disabled) {
+    filter: brightness(110%);
+  }
+
+  &:focus-visible:not(:disabled) {
+    border-color: #264653;
+  }
+}
+</style>
